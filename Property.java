8db49da427afaab7efd9bd7e4448d6335703acfd
HNDThuan <thuan.huynhnguyen.cit20@eiu.edu.vn>
2025-01-09 15:24:37 +0700

    Implement User and Property classes
    Create User and Property classes.
    Create addUser() and resetPassword() method for User class.

---
+++ Property.java
@@ -0,0 +1,11 @@
+public class Property {
+    String propertyID;
+    String description;
+    String ownerID;
+
+    public Property(String id, String description, String ownerID){
+        this.propertyID = id;
+        this.description = description;
+        this.ownerID = ownerID;
+    }
+}
